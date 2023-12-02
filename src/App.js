26bc9cf698efe1f7deac9a25d9c4b157b186e04b
TORVIICT <victormanuelrealpepopayan@gmail.com>
2023-12-02 15:51:00 -0500

    comienzo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 function App() {
+
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return 
+    <div>
+      
+      <h1>GENIMAGE</h1>
+
+      <form>
+        <label>Inserte URL o propmt: 
+          <input type="text" value={value} />
+        </label>
+        <button type="submit">ANALISIS</button>
+        <button type="submit">GENERAR</button>
+      </form>
+    
+      
+    
+    </div>;
 }
 
 export default App;
