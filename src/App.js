a171e0ae09bbbe3ba3a1fe0429f94094f15b055d
TORVIICT <victormanuelrealpepopayan@gmail.com>
2023-12-02 16:15:43 -0500

    arreglo

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 
 function App() {
 
-  const value = 'World';
-  return 
-    <div>
+  return <div>
       
-      <h1>GENIMAGE</h1>
+          <h1>GENIMAGE</h1>
 
-      <form>
-        <label>Inserte URL o propmt: 
-          <input type="text" value={value} />
-        </label>
-        <button type="submit">ANALISIS</button>
-        <button type="submit">GENERAR</button>
-      </form>
-    
-      
-    
-    </div>;
+          <form>
+            <label>Inserte URL o propmt: <br/>
+              <input type="text" name="name" />
+            </label>
+            <br/>
+            <button type="submit">ANALISIS</button>
+            <button type="submit">GENERAR</button>
+          </form>
+        
+          
+        
+        </div>;
 }
 
 export default App;
